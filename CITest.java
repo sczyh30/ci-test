52aa68d8aa3c9467b5a962c203b7d565953afc04
sczyh30 <sczyh16@gmail.com>
2015-11-24 21:52:43 +0800

    update

--- CITest.java
+++ CITest.java
@@ -1,7 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import redis.clients.jedis.Jedis;
+
 public class CITest {
+
+    Jedis jedis = new Jedis("localhost");
+
+    public static void testRedis() {
+        System.out.println(jedis.ping());
+    }
+
+    public static Optional<String> getStringData(String s) {
+        return Optional.ofNullable(jedis.get(s));
+    }
+
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         list.add(13);
@@ -9,5 +22,7 @@ public class CITest {
         list.add(6);
         list.add(9);
         list.forEach(System.out::println);
+
+        testRedis();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ci-test
-Continuous Integration test
+# Continuous Integration Test
+![Build Status]](https://api.travis-ci.org/sczyh30/ci-test.svg?branch=master)
+Gradle Build Integration using Travis CI
